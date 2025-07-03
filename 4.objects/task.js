1e3e5989a98fd102690dd2e0364d3517b4967c26
Fedymini <fedymini@example.com>
2025-07-03 17:23:28 +0300

    задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,25 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject. = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  this.marks.push(...marks);
+  return this.marks;
 }
 
 Student.prototype.getAverage = function () {
-  
+  return this.marks.reduce((acc, mark) => acc + mark , 0) / this.marks.length
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.exclude = reason;
 }
